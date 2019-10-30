94904f8277bd13364917854e0abb2bbd1e3b80b1
gregoriusjimmy <gregoriusjimmy@gmail.com>
2019-10-30 18:02:53 +0700

    adding boundary-box for faceRecognition

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,10 +1,26 @@
 import React from 'react'
+import './FaceRecognition.css'
 
-const FaceRecognition = ({ imageUrl }) => {
+const FaceRecognition = ({ imageUrl, box }) => {
   return (
     <div className='center'>
       <div className='absolute mt2'>
-        <img src={imageUrl} alt='' width='500px' height='auto' />
+        <img
+          id='inputImage'
+          src={imageUrl}
+          alt=''
+          width='500px'
+          height='auto'
+        />
+        <div
+          className='bounding-box'
+          style={{
+            top: box.topRow,
+            left: box.leftCol,
+            right: box.rightCol,
+            bottom: box.bottomRow
+          }}
+        ></div>
       </div>
     </div>
   )
