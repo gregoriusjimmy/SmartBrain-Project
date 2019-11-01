01b377e8ac289d71c02ee1685ba8878d51b77864
gregoriusjimmy <gregoriusjimmy@gmail.com>
2019-11-01 18:22:34 +0700

    finishing sign in,register form and routing

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ class App extends Component {
       imageUrl: '',
       box: {},
       route: 'signin',
-      isSignedIn: 'false'
+      isSignedIn: false
     }
   }
   calculateFaceLocation = (data) => {
@@ -43,7 +43,6 @@ class App extends Component {
     const image = document.getElementById('inputImage')
     const width = Number(image.width)
     const height = Number(image.height)
-    console.log(width, height)
     return {
       leftCol: clarifaiFace.left_col * width,
       topRow: clarifaiFace.top_row * height,
@@ -53,7 +52,6 @@ class App extends Component {
   }
 
   displayFaceBox = (box) => {
-    console.log(box)
     this.setState({ box: box })
   }
 
@@ -79,11 +77,15 @@ class App extends Component {
   }
 
   render() {
+    const { isSignedIn, imageUrl, route, box } = this.state
     return (
       <div className='App'>
         <Particles className='particles' params={particlesOptions} />
-        <Navigation onRouteChange={this.onRouteChange} />
-        {this.state.route === 'home' ? (
+        <Navigation
+          isSignedIn={isSignedIn}
+          onRouteChange={this.onRouteChange}
+        />
+        {route === 'home' ? (
           <div>
             <Logo />
             <Rank />
@@ -91,12 +93,9 @@ class App extends Component {
               onInputChange={this.onInputChange}
               onButtonSubmit={this.onButtonSubmit}
             />
-            <FaceRecognition
-              box={this.state.box}
-              imageUrl={this.state.imageUrl}
-            />
+            <FaceRecognition box={box} imageUrl={imageUrl} />
           </div>
-        ) : this.state.route === 'signin' ? (
+        ) : route === 'signin' ? (
           <Signin onRouteChange={this.onRouteChange} />
         ) : (
           <Register onRouteChange={this.onRouteChange} />
