6f5c593bf03ade3dd500e8abc2fa14f634b9f211
gregoriusjimmy <gregoriusjimmy@gmail.com>
2019-11-06 20:54:56 +0700

    changing background color

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -4,20 +4,13 @@ import './ImageLinkForm.css'
 const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
   return (
     <div>
-      <p className='f3'>
-        {'This Magic Brain will detect facces in your pictures. Give it a try.'}
-      </p>
+      <p className='f3'>{'This Magic Brain will detect faces in your pictures. Give it a try.'}</p>
       <div className='center'>
         <div className='form center pa4 br3 shadow-5'>
-          <input
-            className='f4 pa2 w-70 center'
-            type='text'
-            onChange={onInputChange}
-          />
+          <input className='f4 pa2 w-70 center' type='text' onChange={onInputChange} />
           <button
             className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
-            onClick={onButtonSubmit}
-          >
+            onClick={onButtonSubmit}>
             Detect
           </button>
         </div>
